cd7bd081dde494b7307c4743d0547ef86f2eaee2
Elyes-Ferjani <officialef@hotmail.com>
2021-07-28 00:25:43 +0200

    Add Media Query

--- client/assets/logo/Logo.js
+++ client/assets/logo/Logo.js
@@ -1,8 +1,18 @@
 import React, { useRef, useLayoutEffect } from 'react'
-import { View, Image, StyleSheet } from 'react-native'
+import { View, Image, Text, StyleSheet } from 'react-native'
+import "@expo/match-media"
+import { useMediaQuery } from 'react-responsive'
 import { TweenMax } from 'gsap'
+
 const Logo = () => {
+    const isSmallScreen = useMediaQuery({    
+        query: "(max-device-height: 730px)"  
+    });
+    const xSmallScreen = useMediaQuery({    
+        query: "(max-device-height: 645px)"  
+    });
     let doggo = useRef(null)
+    
     useLayoutEffect(() => {
         TweenMax.from(doggo, 0.5, {
              opacity: 0
@@ -20,10 +30,15 @@ const Logo = () => {
 
     return(
         <View style={styles.container}>
+            {!isSmallScreen ?
             <Image 
             ref={el => doggo = el}
             style={{width: '10rem', height: '10rem'}}
-            source={require('./logoo.svg')} />
+            source={require('./logoo.svg')} /> :
+            xSmallScreen ?
+            <View ref={el => doggo = el} /> :
+            <Text ref={el => doggo = el} style={styles.loginText}>Log in</Text> 
+            }
         </View>
     )
 }
@@ -32,6 +47,11 @@ const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
         flex: 1,
+    },
+    loginText: {
+        marginTop: "10%",
+        fontSize: '2rem',
+        fontWeight: 'bold'
     }
 })
 
