bf603e81b1f5ed06f85755fa54876a87ea06bcd7
Elyes-Ferjani <officialef@hotmail.com>
2021-07-27 12:32:58 +0200

    App Logo

--- client/assets/logo/Logo.js
+++ client/assets/logo/Logo.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useRef, useLayoutEffect } from 'react'
 import { View, Image, StyleSheet } from 'react-native'
-
+import { TweenMax } from 'gsap'
 const Logo = () => {
+    let doggo = useRef(null)
+    useLayoutEffect(() => {
+        TweenMax.from(doggo, 0.5, {
+             opacity: 0
+        }).then(() => {
+            TweenMax.fromTo(doggo, 1, {
+                rotation: 0
+            }, {
+                rotateX: 20,
+                delay: 1,
+                repeat: -1,
+                yoyo: true,
+            })
+        })
+    }, [])
 
     return(
         <View style={styles.container}>
             <Image 
+            ref={el => doggo = el}
             style={{width: '10rem', height: '10rem'}}
-            source={require('./logo.png')} />
+            source={require('./logoo.svg')} />
         </View>
     )
 }
