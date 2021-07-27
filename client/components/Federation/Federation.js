b3f985731f1de56fab59fe1a3d583bbe2ca9fd0f
Elyes-Ferjani <officialef@hotmail.com>
2021-07-27 12:33:37 +0200

    Google and Facebook

--- client/components/Federation/Federation.js
+++ client/components/Federation/Federation.js
@@ -1,17 +1,23 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native'
 const Federation = () => {
     return (
         <View style={styles.container}>
             <TouchableOpacity
                 style={styles.google}
             >
+                <Image 
+                style={{width: '2rem', height: '2rem'}}
+                source={require('../../assets/icons/google.png')} />
                 <Text style={styles.googleText}>Sign In With Google</Text>
             </TouchableOpacity>
             <TouchableOpacity
                 style={styles.facebook}
             >
-
+                <Image 
+                style={{width: '1rem', height: '2rem'}}
+                source={require('../../assets/icons/facebook.png')} />
+                <Text style={styles.facebookText}>Sign In With Facebook</Text>
             </TouchableOpacity>
         </View>
     )
@@ -25,22 +31,38 @@ const styles = StyleSheet.create({
  
     },
     google: {
+        alignItems: 'center',
+        justifyContent: 'center',
         width: '50%',
         height: '3rem',
         borderWidth: 1,
         borderColor: "#a9a9a9",
         marginTop: 10,
         marginBottom: 10,
-        borderRadius: 5
+        borderRadius: 5,
+        flexDirection: "row",
+        flex: 1
     },
+    googleText: {
+        fontSize: '0.6rem',
+        marginLeft: 2
+    },  
     facebook: {
+        alignItems: 'center',
+        justifyContent: 'center',
         width: '50%',
         height: '3rem',
         borderWidth: 1,
         borderColor: "#a9a9a9",
         marginTop: 10,
         marginBottom: 10,
-        borderRadius: 5
+        borderRadius: 5,
+        flexDirection: "row",
+        flex: 1
+    },
+    facebookText: {
+        fontSize: '0.6rem',
+        marginLeft: 2
     }
 })
 
