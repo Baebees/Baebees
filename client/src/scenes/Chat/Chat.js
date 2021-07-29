4304f58d10b2fae40a2968f51cf8f5e7b72b0426
Elyes-Ferjani <officialef@hotmail.com>
2021-07-29 21:39:10 +0200

    Adding Chat screen

---
+++ client/src/scenes/Chat/Chat.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text} from 'react-native'
+
+const Chat = () => {
+    return(
+        <View>
+            <Text>
+                chat list
+            </Text>
+        </View>
+    )
+}
+
+export default Chat
