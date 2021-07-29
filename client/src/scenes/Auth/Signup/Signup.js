f928ba50a8e49fe9dd1d835f02e6833d18d8ff3c
Elyes-Ferjani <officialef@hotmail.com>
2021-07-29 21:37:47 +0200

    Changin directory

---
+++ client/src/scenes/Auth/Signup/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native';
+import Register from '../../../components/Register'
+
+const Signup = () => {
+    return (
+        <View>
+            <Register />
+        </View>
+    )
+}
+
+export default Signup
