458068e9be6671405771f388f4ecedfefb44c158
Elyes-Ferjani <officialef@hotmail.com>
2021-07-29 21:38:54 +0200

    Adding Post screen

---
+++ client/src/scenes/Post/Post.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Post = (props) => {
+    return(
+        <View>
+            <Text>
+                Post
+            </Text>
+        </View>
+    )
+}
+
+export default Post
