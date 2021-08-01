e940364074262056d76da27380a6d9f5091bc1d4
Elyes-Ferjani <officialef@hotmail.com>
2021-08-01 02:46:53 +0200

    Adding colors

--- client/src/theme/colors.js
+++ client/src/theme/colors.js
@@ -2,5 +2,8 @@ export const colors = {
     gray: '#F0F2F5',
     darkGray: '#a9a9a9',
     blue: '#6495ed',
-    black: '#000'
+    black: '#000',
+    lightBlack: '#474342',
+    reddish: '#FF3333',
+    white: '#EAEAEA'
 }
