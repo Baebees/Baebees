b0b1702da7d7fc899c1e3fff9ff57d2b20831ef9
Elyes-Ferjani <officialef@hotmail.com>
2021-08-01 02:46:30 +0200

    Working on posts view In Home Page

--- client/src/scenes/Home/Home.js
+++ client/src/scenes/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import Bee from '../../components/Bee'
+import VideoPlayer from '../../components/VideoPlayer'
 const Home = (props) => {
 
     return(
         <View>
-            <Text>home</Text>
+            <Bee>
+                <VideoPlayer />
+            </Bee>
         </View>
     )
 }
