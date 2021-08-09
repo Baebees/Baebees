9b1a63880b90ff902ef8befd5069584e11792778
Elyes-Ferjani <officialef@hotmail.com>
2021-08-09 18:42:27 +0200

    Search View Update

--- client/src/scenes/Search/Search.js
+++ client/src/scenes/Search/Search.js
@@ -1,14 +1,33 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, Image, Dimensions, StyleSheet } from 'react-native'
+import SearchComponent from '../../components/SearchComponent'
+import User from '../../components/User'
+const { height } = Dimensions.get('screen')
 
 const Search = (props) => {
-    return(
+    const [isEmpty, setEmpty] = useState(false)
+    return (
         <View>
-            <Text>
-                Search
-            </Text>
+            <SearchComponent />
+            {isEmpty ?
+                <View style={styles.bowl}>
+                    <Image
+                        style={{ height: 100, width: 100 }}
+                        source={require('../../../assets/icons/emptybowl.png')}
+                    />
+                </View> :
+                <User {...props} path="UserProfile"/>
+            }
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    bowl: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: height * 0.7
+    }
+})
 
 export default Search
