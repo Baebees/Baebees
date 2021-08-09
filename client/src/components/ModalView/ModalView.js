0a095b705d15c002ae4cd2a1e1902df1e1b523cf
Elyes-Ferjani <officialef@hotmail.com>
2021-08-09 18:39:30 +0200

    Adding modal component

---
+++ client/src/components/ModalView/ModalView.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { View, Image, StyleSheet, Dimensions } from 'react-native'
+import FontIcon from 'react-native-vector-icons/FontAwesome5'
+import { colors } from '../../theme/colors'
+import Comment from '../../components/Comment'
+const { height, width } = Dimensions.get('screen')
+
+const ModalView = ({ route, navigation }) => {
+    const { item } = route.params
+    return (
+        <View style={styles.container}>
+            <Image
+                style={{ height: height * 0.5, width: width }}
+                source={item.src}
+            />
+            <View style={styles.reaction}>
+                <FontIcon
+                    style={styles.icon}
+                    name="heart"
+                    size={25}
+                />
+                <FontIcon
+                    style={styles.icon}
+
+                    name="comment"
+                    size={25}
+                />
+            </View>
+            <View>
+                <Comment />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    reaction: {
+        flexDirection: 'row',
+        backgroundColor: colors.clearWhite
+    },
+    icon: {
+        marginLeft: 10,
+        marginTop: 5,
+    },
+})
+
+export default ModalView
