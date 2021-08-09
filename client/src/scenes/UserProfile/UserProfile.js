be95c9ade253a8be1b1297d29d769b10eeabbd6d
Elyes-Ferjani <officialef@hotmail.com>
2021-08-09 18:41:15 +0200

    Adding UserProfile

---
+++ client/src/scenes/UserProfile/UserProfile.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { View, TouchableOpacity, StyleSheet } from 'react-native'
+import ProfileInfo from '../../components/ProfileInfo'
+import MyBees from '../../components/MyBees'
+import FontIcon from 'react-native-vector-icons/FontAwesome5'
+
+const UserProfile = (props) => {
+    return (
+        <View>
+            <ProfileInfo />
+            <View style={styles.actions}>
+                <TouchableOpacity>
+                    <FontIcon
+                        name="user-friends"
+                        size={25}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <FontIcon
+                        name="envelope"
+                        size={25}
+                        solid
+                    />
+                </TouchableOpacity>
+            </View>
+            <MyBees { ...props }/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    actions: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 50,
+        backgroundColor: 'white',
+        marginTop: 2,
+        marginBottom: 2,
+        paddingVertical: 10
+    }
+})
+
+export default UserProfile
