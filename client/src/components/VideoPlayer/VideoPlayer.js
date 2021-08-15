8751a0ec070db1de98c3299ee17413d99a666a4b
Elyes-Ferjani <officialef@hotmail.com>
2021-08-15 13:01:50 +0200

    Switch Video Player To Expo AV

--- client/src/components/VideoPlayer/VideoPlayer.js
+++ client/src/components/VideoPlayer/VideoPlayer.js
@@ -1,42 +1,34 @@
-import React from 'react';
+import React, { useState, useRef }from 'react';
 import { View, StyleSheet, Button, Text } from 'react-native';
 import { Video, AVPlaybackStatus } from 'expo-av';
-import VideoPlayer from 'expo-video-player';
 import { Dimensions } from 'react-native';
 
-const videoPlayer = () => {
-  const video = React.useRef(null);
-  const [status, setStatus] = React.useState({});
+const VideoPlayer = () => {
+  const video = useRef(null);
+  const [status, setStatus] = useState({});
   return (
-    <View style={styles.container}>
-      <VideoPlayer
-      defaultControlsVisible={true}
-      //  showControlsOnLoad={true}
-       videoProps={{
-        shouldPlay: false,
-        resizeMode: Video.RESIZE_MODE_CONTAIN,
-        source: {
-          uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-        },
-      }}
-      icon={{
-        play: <Text style={{ color: '#FFF' }}>PLAY</Text>,
-        pause: <Text style={{ color: '#FFF' }}>PAUSE</Text>,
-      }}
-      style={{ height: Dimensions.get('screen').height/2 }}
-
+    <View>
+      <Video
+        ref={video}
+        source={{
+          uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      height: Dimensions.get('screen').height/2
-    },
-    video: {
-        maxHeight: Dimensions.get('screen').height/2
-    }
+  container: {
+    height: Dimensions.get('screen').height/2
+  },
+  video: {
+      maxHeight: Dimensions.get('screen').height/2
+  }
 })
 
-export default videoPlayer
+export default VideoPlayer
