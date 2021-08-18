ea32f606496c2b9f14d29ef6ee03d3450838a982
Elyes-Ferjani <officialef@hotmail.com>
2021-08-18 18:09:30 +0200

    Adding Track view

---
+++ client/src/scenes/Track/Track.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+// import MapView from "react-native-maps"
+const Track = (props) => {
+    return (
+        <View>
+            {/* <MapView style={styles.map} /> */}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
+
+export default Track
