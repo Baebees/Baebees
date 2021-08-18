3e15b0e6f8f969effdefa7b6f88346a30333d80d
Elyes-Ferjani <officialef@hotmail.com>
2021-08-18 18:11:19 +0200

    Modifying Subscription View

--- client/src/scenes/Subscription/Subscription.js
+++ client/src/scenes/Subscription/Subscription.js
@@ -1,12 +1,79 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Button } from 'react-native'
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native'
+const { height, width } = Dimensions.get('window')
 
-const Subscription = (props) => {
-    return(
-        <View>
-
+const Subscription = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.description}>
+                <Image
+                    style={{ height: height * 0.2, width: height * 0.2 }}
+                    source={require('../../../assets/icons/cat.png')}
+                />
+            </View>
+            <View style={styles.confirmation}>
+                <Text style={styles.reminder}>
+                    Reminder
+                </Text>
+                <View>
+                <Text style={styles.txt}>
+                    By choosing confirm, you are accepting to subscribe for the offer given by Baebees.{"\n"}We are responsible for:{"\n"}
+                </Text>
+                <Text style={styles.txt}>
+                ●&nbsp;saving your Pet name along with an identifier with the owner phone number{"\n"}
+                </Text>
+                <Text style={styles.txt}>
+                ●&nbsp;providing the owner with the demanded informations{"\n"}
+                </Text>
+                <Text style={styles.txt}>
+                ●&nbsp;not to share your personal informations{"\n"}
+                </Text>
+                <Text style={styles.txt}>
+                ●&nbsp;contribute in the process of finding your missing Pet{"\n"}
+                </Text>
+                </View>
+                <View style={styles.button}>
+                <Button
+                    title="Confirm"
+                    accessibilityLabel="Learn more about this subscription"
+                    onPress={()=> navigation.navigate('Payment')}
+                />
+            </View>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
+    },
+    description: {
+        alignItems: 'center',
+        // marginTop: height * 0.1
+    },
+    confirmation: {
+        height: height * 0.5,
+        width: width * 0.8,
+        alignItems: "center",
+        justifyContent: 'space-evenly'
+    },
+    button: {
+        width: width * 0.6,
+        height: 30,
+    },
+    reminder: {
+        fontSize: 30,
+        fontWeight: '600'
+    },
+    txt: {
+        fontSize: 15,
+        fontWeight: '500',
+        lineHeight: 25,
+    }
+})
 
 export default Subscription
