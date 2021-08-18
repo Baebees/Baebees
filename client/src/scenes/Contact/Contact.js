cbb282698e80ba3dfbdda6cc4a971e65fa398d6b
Elyes-Ferjani <officialef@hotmail.com>
2021-08-18 18:08:39 +0200

    Adding Contact View

---
+++ client/src/scenes/Contact/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react' 
+import { View, StyleSheet } from 'react-native'
+
+const Contact = (props) => {
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
+
+export default Contact
