84d8786470c906f63064f25660aa1f840310ad6c
Elyes-Ferjani <officialef@hotmail.com>
2021-08-31 10:53:53 +0200

    Modifying Bees Routing

--- client/src/components/Bees/Bees.js
+++ client/src/components/Bees/Bees.js
@@ -2,128 +2,88 @@ import React from 'react'
 import { TouchableOpacity, View, StyleSheet, ScrollView } from 'react-native'
 import { Avatar, Badge, Icon, withBadge } from 'react-native-elements'
 import { colors } from '../../theme/colors'
-const item = {src: "https://img.freepik.com/vecteurs-libre/homme-affaires-caractere-avatar-isole_24877-60111.jpg?size=338&ext=jpg"}
+const item = { src: "https://img.freepik.com/vecteurs-libre/homme-affaires-caractere-avatar-isole_24877-60111.jpg?size=338&ext=jpg" }
 const Bees = ({ navigation }) => {
     return (
         <View style={styles.layer}>
-        <ScrollView
-            horizontal={true}
-            style={styles.container}
-            showsHorizontalScrollIndicator={false}
-        >
-            <TouchableOpacity style={styles.bee} 
-            onPress={()=> navigation.navigate('Media', { from: 'Home', item })}
+            <ScrollView
+                horizontal={true}
+                style={styles.container}
+                showsHorizontalScrollIndicator={false}
             >
-                <Avatar
-                    rounded
-                    containerStyle={{ borderWidth: 3, borderColor: colors.gray, borderStyle:'solid' }}
-                    source={{
-                        uri: 'https://randomuser.me/api/portraits/men/41.jpg',
-                    }}
-                    size="medium"
-                />
-
-                <Badge
-                    status="success"
-                    containerStyle={styles.status}
-                    textStyle={{ fontWeight: 40 }}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.bee}>
-                <Avatar
-                    rounded
-                    containerStyle={{ borderWidth: 3, borderColor: colors.gray, borderStyle:'solid' }}
-                    source={{
-                        uri: 'https://randomuser.me/api/portraits/men/41.jpg',
-                    }}
-                    size="medium"
-                />
-
-                <Badge
-                    status="success"
-                    containerStyle={styles.status}
-                    textStyle={{ fontWeight: 40 }}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.bee}>
-                <Avatar
-                    rounded
-                    containerStyle={{ borderWidth: 3, borderColor: colors.gray, borderStyle:'solid' }}
-                    source={{
-                        uri: 'https://randomuser.me/api/portraits/men/41.jpg',
-                    }}
-                    size="medium"
-                />
-
-                <Badge
-                    status="success"
-                    containerStyle={styles.status}
-                    textStyle={{ fontWeight: 40 }}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.bee}>
-                <Avatar
-                    rounded
-                    containerStyle={{ borderWidth: 3, borderColor: colors.gray, borderStyle:'solid' }}
-                    source={{
-                        uri: 'https://randomuser.me/api/portraits/men/41.jpg',
-                    }}
-                    size="medium"
-                />
-
-                <Badge
-                    status="success"
-                    containerStyle={styles.status}
-                    textStyle={{ fontWeight: 40 }}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.bee}>
-                <Avatar
-                    rounded
-                    containerStyle={{ borderWidth: 3, borderColor: colors.gray, borderStyle:'solid' }}
-                    source={{
-                        uri: 'https://randomuser.me/api/portraits/men/41.jpg',
-                    }}
-                    size="medium"
-                />
-
-                <Badge
-                    status="success"
-                    containerStyle={styles.status}
-                    textStyle={{ fontWeight: 40 }}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.bee}>
-                <Avatar
-                    rounded
-                    containerStyle={{ borderWidth: 3, borderColor: colors.gray, borderStyle:'solid' }}
-                    source={{
-                        uri: 'https://randomuser.me/api/portraits/men/41.jpg',
-                    }}
-                    size="medium"
-                />
-
-                <Badge
-                    status="success"
-                    containerStyle={styles.status}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.bee}>
-                <Avatar
-                    rounded
-                    containerStyle={{ borderWidth: 3, borderColor: colors.gray, borderStyle:'solid' }}
-                    source={{
-                        uri: 'https://randomuser.me/api/portraits/men/41.jpg',
-                    }}
-                    size="medium"
-                />
-
-                <Badge
-                    status="success"
-                    containerStyle={styles.status}
-                />
-            </TouchableOpacity>
-        </ScrollView>
+                <TouchableOpacity style={styles.bee}
+                    onPress={() => navigation.navigate('LostMedia', { from: 'Home', item })}
+                >
+                    <Avatar
+                        rounded
+                        containerStyle={{ borderWidth: 3, borderColor: colors.blue, borderStyle: 'solid' }}
+                        source={{
+                            uri: 'https://randomuser.me/api/portraits/men/41.jpg',
+                        }}
+                        size="medium"
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.bee}>
+                    <Avatar
+                        rounded
+                        containerStyle={{ borderWidth: 3, borderColor: colors.blue, borderStyle: 'solid' }}
+                        source={{
+                            uri: 'https://randomuser.me/api/portraits/men/41.jpg',
+                        }}
+                        size="medium"
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.bee}>
+                    <Avatar
+                        rounded
+                        containerStyle={{ borderWidth: 3, borderColor: colors.blue, borderStyle: 'solid' }}
+                        source={{
+                            uri: 'https://randomuser.me/api/portraits/men/41.jpg',
+                        }}
+                        size="medium"
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.bee}>
+                    <Avatar
+                        rounded
+                        containerStyle={{ borderWidth: 3, borderColor: colors.blue, borderStyle: 'solid' }}
+                        source={{
+                            uri: 'https://randomuser.me/api/portraits/men/41.jpg',
+                        }}
+                        size="medium"
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.bee}>
+                    <Avatar
+                        rounded
+                        containerStyle={{ borderWidth: 3, borderColor: colors.blue, borderStyle: 'solid' }}
+                        source={{
+                            uri: 'https://randomuser.me/api/portraits/men/41.jpg',
+                        }}
+                        size="medium"
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.bee}>
+                    <Avatar
+                        rounded
+                        containerStyle={{ borderWidth: 3, borderColor: colors.blue, borderStyle: 'solid' }}
+                        source={{
+                            uri: 'https://randomuser.me/api/portraits/men/41.jpg',
+                        }}
+                        size="medium"
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.bee}>
+                    <Avatar
+                        rounded
+                        containerStyle={{ borderWidth: 3, borderColor: colors.blue, borderStyle: 'solid' }}
+                        source={{
+                            uri: 'https://randomuser.me/api/portraits/men/41.jpg',
+                        }}
+                        size="medium"
+                    />
+                </TouchableOpacity>
+            </ScrollView>
         </View>
     )
 }
