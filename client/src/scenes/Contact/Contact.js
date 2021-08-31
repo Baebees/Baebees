910cfa96bbe3df389e5acb9f28bdeb46cf3a7e2e
Elyes-Ferjani <officialef@hotmail.com>
2021-08-31 10:55:24 +0200

    Adding Contact Us View

--- client/src/scenes/Contact/Contact.js
+++ client/src/scenes/Contact/Contact.js
@@ -1,10 +1,39 @@
-import React from 'react' 
+import React, { useState } from 'react'
 import { View, StyleSheet } from 'react-native'
+import FontIcon from 'react-native-vector-icons/FontAwesome5'
+import { ListItem } from 'react-native-elements'
+import { colors } from '../../theme/colors'
 
 const Contact = (props) => {
-    return(
+    return (
         <View>
-
+            <ListItem bottomDivider>
+                <View style={{ width: 25 }}>
+                    <FontIcon
+                        name="envelope-open"
+                        color={colors.lightBlack}
+                        backgroundColor="transparent"
+                        size={20}
+                        solid
+                    />
+                </View>
+                <ListItem.Content>
+                    <ListItem.Title>E-mail us</ListItem.Title>
+                </ListItem.Content>
+            </ListItem>
+            <ListItem bottomDivider>
+                <View style={{ width: 25 }}>
+                    <FontIcon
+                        name="phone-alt"
+                        color={colors.lightBlack}
+                        backgroundColor="transparent"
+                        size={20}
+                    />
+                </View>
+                <ListItem.Content>
+                    <ListItem.Title>Call us</ListItem.Title>
+                </ListItem.Content>
+            </ListItem>
         </View>
     )
 }
