c80338d715a39c7e397d784b8b353c21eef1e9ca
Elyes-Ferjani <officialef@hotmail.com>
2021-09-04 00:15:18 +0200

    Adding Followers Scene

---
+++ client/src/scenes/Followers/Followers.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+const Followers = ({ route }) => {
+    const { followers, context } = route.params
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Followers
